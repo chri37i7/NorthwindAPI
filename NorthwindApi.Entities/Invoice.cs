d9b6eb956f572fb1ec569cfeff78526ca179d5a5
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-08-17 14:28:20 +0200

    Add project files.

---
+++ NorthwindApi.Entities/Invoice.cs
@@ -0,0 +1,16 @@
+﻿namespace NorthwindApi.Entities
+{
+    public class Invoice
+    {
+        private decimal totalPrice;
+
+        public Invoice(string customerName, decimal extendedPrice, decimal freightPrice)
+        {
+            totalPrice = extendedPrice + freightPrice;
+            CustomerName = customerName;
+        }
+
+        public string CustomerName { get; set; }
+        public decimal TotalPrice => totalPrice;
+    }
+}
